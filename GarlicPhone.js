1e178fba41093a18f7b01ed44208fe192df22dc5
OzelotGamer <136556115+OzelotGamer@users.noreply.github.com>
2024-03-14 19:45:22 +0100

    Update GarlicPhone.js

--- GarlicPhone.js
+++ GarlicPhone.js
@@ -4,17 +4,19 @@ Feel free to suggest changes.
 Im a beginner and this is just a little test Project.
 I will comment on every little detail telling you what it is so even if you're not a programmer you can understand it.
 */
+garinit();
 function garinit(){
 document.querySelectorAll('[role="button"]')[0].id = "glplogo";
-document.getElementByID("glplogo").innerHTML = "GARLIC PHONE";
-var style = document.createElement('style');
-  style.innerHTML += "#glplogo{text-align: center; color = white; font-family: 'Nunito'; font-size: 22px;}";
-  document.body.append(style);
-//Deleting Gartic Phone title and replacing it with Garlic Phone.
+document.getElementById("glplogo").innerHTML = "GARLIC<br>PHONE";
+var shadow = document.createElement("div");
+shadow.id = "glplogoshadow";
+shadow.innerHTML = "GARLIC<br>PHONE";
+document.getElementById().append(shadow);
+
 setInterval(()=>{
-  for(i=0;i<document.getElementsByTagName("iframe").length;i++)
+for(i=0;i<document.getElementsByTagName("iframe").length;i++)
   {
   document.getElementsByTagName("iframe")[i].remove();
   }
- }, 100); //Deleting IFrame ads, because ads are annoying (just like me).
+ }, 100);
 }
