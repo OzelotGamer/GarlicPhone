b0aabfacd00d22e2c6cf82203a3b7dc962d526f6
OzelotGamer <136556115+OzelotGamer@users.noreply.github.com>
2024-03-14 15:53:08 +0100

    Update GarlicPhone.js

--- GarlicPhone.js
+++ GarlicPhone.js
@@ -4,6 +4,7 @@ Feel free to suggest changes.
 Im a beginner and this is just a little test Project.
 I will comment on every little detail telling you what it is so even if you're not a programmer you can understand it.
 */
+function garinit(){
 document.querySelectorAll('[role="button"]')[0].innerHTML = "Garlic Phone"; //Deleting Gartic Phone title and replacing it with Garlic Phone.
 setInterval(()=>{
   for(i=0;i<document.getElementsByTagName("iframe").length;i++)
@@ -11,3 +12,4 @@ setInterval(()=>{
   document.getElementsByTagName("iframe")[i].remove();
   }
  }, 100); //Deleting IFrame ads, because ads are annoying (just like me).
+}
