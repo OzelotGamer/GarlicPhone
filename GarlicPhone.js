615895de9068247ba12cd9d9096f1e52a2b4e897
OzelotGamer <136556115+OzelotGamer@users.noreply.github.com>
2024-03-16 20:39:01 +0100

    Update GarlicPhone.js

--- GarlicPhone.js
+++ GarlicPhone.js
@@ -7,13 +7,22 @@ I will comment on every little detail telling you what it is so even if you're n
 garinit();
 function garinit(){
 document.querySelectorAll('[role="button"]')[0].id = "glplogo";
-document.getElementById("glplogo").innerHTML = "GARLIC<br>PHONE";
+document.getElementById("glplogo").innerHTML = "";
+var main = document.createElement("div");
+main.innerHTML = "GARLIC<br>PHONE";
+main.id = "glplogomain";
+document.getElementById("glplogo").append(main);
 var shadow = document.createElement("div");
+shadow.innerHTML = "GARLIC<br>PHONE";
 shadow.id = "glplogoshadow";
-shadow.innerHTML = "<div id='glplogomain'>GARLIC<br>PHONE</div>";
 document.getElementById("glplogo").append(shadow);
+var secshadow = document.createElement("div");
+secshadow.innerHTML = "GARLIC<br>PHONE";
+secshadow.id = "glplogoshadow";
+secshadow.id = "glplogosecshadow";
+document.getElementById("glplogo").append(secshadow);
 var styles = document.createElement("style");
-styles.innerHTML = "#glplogo{} #glplogomain{color: white; font-size: 25px; position: absolute; top: 0%; left: 50%; transform: translate(-50%, 0%)} #glplogoshadow{color: white; font-size: 25px; position: absolute; top: 0%; left: 50%; transform: translate(-50%, 0%)}";
+styles.innerHTML = "#glplogo{} #glplogomain{z-index: 5; color: #777799; font-size: 27px; position: absolute; top: 10%; left: 50%; transform: translate(-50%, -10%); font-weight: 300;} #glplogoshadow{z-index: 4; color: white; font-size: 28px; position: absolute; top: 10%; left: 50%; transform: translate(-50%, -10%); font-weight: 600;} #glplogosecshadow{z-index: 3; color: #777799; font-size: 29px; position: absolute; top: 10%; left: 50%; transform: translate(-50%, -10%); font-weight: 900;}";
 document.getElementById("glplogo").append(styles);
 
 setInterval(()=>{
